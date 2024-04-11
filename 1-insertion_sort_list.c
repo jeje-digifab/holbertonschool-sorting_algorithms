6c84e7562457aa8e6edd3a36b4437cee274489c8
jeje-digifab <j.romand.digifab@gmail.com>
2024-04-11 13:54:10 +0200

    Betty's correction

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,30 +7,30 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *tmp;
+	listint_t *current, *tmp;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    current = (*list)->next;
-    while (current != NULL)
-    {
-        tmp = current->prev;
-        while (tmp != NULL && tmp->n > current->n)
-        {
-            if (tmp->prev != NULL)
-                tmp->prev->next = current;
-            if (current->next != NULL)
-                current->next->prev = tmp;
-            tmp->next = current->next;
-            current->prev = tmp->prev;
-            current->next = tmp;
-            tmp->prev = current;
-            if (current->prev == NULL)
-                *list = current;
-            print_list(*list);
-            tmp = current->prev;
-        }
-        current = current->next;
-    }
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		tmp = current->prev;
+		while (tmp != NULL && tmp->n > current->n)
+		{
+			if (tmp->prev != NULL)
+				tmp->prev->next = current;
+			if (current->next != NULL)
+				current->next->prev = tmp;
+			tmp->next = current->next;
+			current->prev = tmp->prev;
+			current->next = tmp;
+			tmp->prev = current;
+			if (current->prev == NULL)
+				*list = current;
+			print_list(*list);
+			tmp = current->prev;
+		}
+		current = current->next;
+	}
 }
