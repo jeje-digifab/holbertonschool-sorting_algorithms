b9e5feac998b3e86840734d25f031ddc8de0708b
jeje-digifab <j.romand.digifab@gmail.com>
2024-04-11 13:28:38 +0200

    Fix code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,7 +25,7 @@ void bubble_sort(int *array, size_t size)
 
 	if (!array || size < 2)
 	{
-		return;
+		print_array(array, size);
 	}
 
 	for (i = 0;  i < size - 1; i++)
@@ -37,8 +37,8 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				print_array(array, size);
 			}
 		}
-		print_array(array, size);
 	}
 }
